a3ccbeea58c7e10965c5232bb2c0ef0de69d8b4c
Манашерв Бэн <bsmanasherov@edu.hse.ru>
2026-06-03 04:42:01 +0300

    Реализован класс DietaryRecipe

--- recipes.py
+++ recipes.py
@@ -112,3 +112,18 @@ class ShoppingList:
         new_list = ShoppingList()
         new_list._items = self._items + other._items
         return new_list
+
+
+class DietaryRecipe(Recipe):
+    def __init__(self, title, diet_type, ingredients=None):
+        self.diet_type = diet_type
+        super().__init__(title, ingredients)
+
+
+    def scale(self, ratio: float):
+        scal = super().scale(ratio)
+        return DietaryRecipe(scal.title, self.diet_type, scal.ingredients)
+
+
+    def __str__(self):
+        return f'[{self.diet_type}] {super().__str__()}'
